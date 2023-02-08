aa64bd559d2616c715f9a1eaf776aa019eb85392
iheb19 <iheb.gharbi@esprit.tn>
2023-02-08 23:01:59 +0100

    veriff

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.9-alpine3.13
 LABEL maintainer="londonappdeveloper.com"
 
-ENV PYTHONUNBUFFERED 1  
+ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /tmp/requirements.txt
 COPY ./requirements.dev.txt /tmp/requirements.dev.txt
@@ -9,7 +9,7 @@ COPY ./app /app
 WORKDIR /app
 EXPOSE 8000
 
-ARG DEV=false
+ARG DEV=true
 RUN python -m venv /py && \
     /py/bin/pip install --upgrade pip && \
     apk add --update --no-cache postgresql-client && \
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    "manage for users"
+
+
+    def create_user(self, email, password=None, **extra_fields):
+        """create save return new user"""
+
+        user=self.model(email=email, **extra_fields)
+        user.setpassword(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """user in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    object = UserManager()
+
+    USERNAME_FIELD = 'email'
